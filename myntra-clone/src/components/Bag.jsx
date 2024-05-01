b9c74e529dcbc74558c9d16537b2e278f4881696
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-05-01 20:43:06 +0530

    regular commit, done regular work on project

---
+++ myntra-clone/src/components/Bag.jsx
@@ -0,0 +1,23 @@
+import Header from "../src/components/Header"
+import Footer from "./components/Footer"
+
+const Bag = () => {
+return <>
+
+ <Header></Header>
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container">
+        </div>
+        <div className="bag-summary">
+        </div>
+
+      </div>
+    </main>
+   <Footer></Footer>
+
+
+</>
+};
+
+export default Bag;
